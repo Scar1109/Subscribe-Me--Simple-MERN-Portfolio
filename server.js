484e54dcd7e6d43e1d232565db11bfb432da51cb
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-01-19 04:35:16 +0530

    updates

--- server.js
+++ server.js
@@ -7,5 +7,7 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 app.listen(port, () =>
-    console.log(`Server running on port ${port} with nodemon`)
+    console.log(`Server running on port ${port}`)
 );
+
+
